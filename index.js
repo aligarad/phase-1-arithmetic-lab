73b80d21ae3152e3743ecb0c8986f3bd02b96f3d
kenhunja <garadcali@gmail.com>
2022-08-20 11:44:46 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-// Write your code here
+// Write your code here!
+ let num1 = 2;
+ let num2 = 31;
+
+ const multiply = num1*num2;
+
+ const random = Math.floor(Math.random() * 10) + 1;
+
+ const num3 = 9;
+ const num4 = 5;
+
+ const mod = num3 % num4 ;
+
+ const max = Math.max( 18, 17, 20);
